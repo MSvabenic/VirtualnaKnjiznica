60630cb86840009220b05f31a915eb65e2f2845e
Marko <criking92@gmail.com>
2017-09-24 18:08:54 +0200

    Dodan viewModel za autora,view i controller. Kontroler namjesten za spremanje. Kreirana forma u viewu.

---
+++ VirtualnaKnjižnica/Controllers/AutorController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VirtualnaKnjižnica.Models;
+using VirtualnaKnjižnica.ViewModels;
+
+namespace VirtualnaKnjižnica.Controllers
+{
+    public class AutorController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public AutorController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult DodajAutora()
+        {
+            var viewModel = new AutorViewModel();
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult DodajAutora(AutorViewModel viewModel)
+        {
+            var autor = new Autor
+            {
+                AutorIme = viewModel.Ime,
+                AutorPrezime = viewModel.Prezime
+            };
+            _context.Autor.Add(autor);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
